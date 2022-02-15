7bb012c51bc544054b54916c943885ffe4557c3d
Bastien HUBERT <hbast4@gmail.com>
2022-02-15 09:34:03 +0100

    exercice 4

--- src/main/java/ensta/model/Player.java
+++ src/main/java/ensta/model/Player.java
@@ -1,103 +1,101 @@
 package ensta.model;
 
-import java.io.Serializable;
-import java.util.List;
-
 import ensta.model.ship.AbstractShip;
 import ensta.util.Orientation;
 import ensta.view.InputHelper;
+import java.io.Serializable;
+import java.util.List;
 
 public class Player {
-	/*
-	 * ** Attributs
-	 */
-	private Board board;
-	protected Board opponentBoard;
-	private int destroyedCount;
-	protected AbstractShip[] ships;
-	private boolean lose;
+    /*
+     * ** Attributs
+     */
+    private Board board;
+    protected Board opponentBoard;
+    private int destroyedCount;
+    protected AbstractShip[] ships;
+    private boolean lose;
 
-	/*
-	 * ** Constructeur
-	 */
-	public Player(Board board, Board opponentBoard, List<AbstractShip> ships) {
-		this.setBoard(board);
-		this.ships = ships.toArray(new AbstractShip[0]);
-		this.opponentBoard = opponentBoard;
-	}
+    /*
+     * ** Constructeur
+     */
+    public Player( Board board, Board opponentBoard, List<AbstractShip> ships ) {
+        this.setBoard( board );
+        this.ships = ships.toArray( new AbstractShip[0] );
+        this.opponentBoard = opponentBoard;
+    }
 
-	/*
-	 * ** Méthodes
-	 */
+    /*
+     * ** Méthodes
+     */
 
-	/**
-	 * Read keyboard input to get ships coordinates. Place ships on given
-	 * coodrinates.
-	 */
-	public void putShips() {
-		boolean done = false;
-		int i = 0;
+    /**
+     * Read keyboard input to get ships coordinates. Place ships on given
+     * coodrinates.
+     */
+    public void putShips() {
+        boolean done = false;
+        int i = 0;
 
-		do {
-			AbstractShip ship = ships[i];
-			String msg = String.format("placer %d : %s(%d)", i + 1, ship.getName(), ship.getLength());
-			System.out.println(msg);
-			InputHelper.ShipInput res = InputHelper.readShipInput();
-			// TODO set ship orientation
-			// TODO put ship at given position
-			// TODO when ship placement successful
-			++i;
-			done = i == 5;
+        do {
+            AbstractShip ship = ships[i];
+            String msg = String.format( "placer %d : %s(%d)", i + 1, ship.getName(), ship.getLength() );
+            System.out.println( msg );
+            InputHelper.ShipInput res = InputHelper.readShipInput();
+            ship.setOrientation( Orientation.StringToOrientation( res.orientation ) );
+            if ( board.putShip( ship, new Coords( res.x, res.y ) ) )
+                ++i;
+            done = i == 5;
 
-			board.print();
-		} while (!done);
-	}
+            board.print();
+        } while ( !done );
+    }
 
-	public Hit sendHit(Coords coords) {
-		boolean done = false;
-		Hit hit = null;
+    public Hit sendHit( Coords coords ) {
+        boolean done = false;
+        Hit hit = null;
 
-		do {
-			System.out.println("où frapper?");
-			InputHelper.CoordInput hitInput = InputHelper.readCoordInput();
-			// TODO call sendHit on this.opponentBoard
+        do {
+            System.out.println( "où frapper?" );
+            InputHelper.CoordInput hitInput = InputHelper.readCoordInput();
+            // TODO call sendHit on this.opponentBoard
 
-			// TODO : Game expects sendHit to return BOTH hit result & hit coords.
-			// return hit is obvious. But how to return coords at the same time ?
-		} while (!done);
+            // TODO : Game expects sendHit to return BOTH hit result & hit coords.
+            // return hit is obvious. But how to return coords at the same time ?
+        } while ( !done );
 
-		return hit;
-	}
+        return hit;
+    }
 
-	public AbstractShip[] getShips() {
-		return ships;
-	}
+    public AbstractShip[] getShips() {
+        return ships;
+    }
 
-	public void setShips(AbstractShip[] ships) {
-		this.ships = ships;
-	}
+    public void setShips( AbstractShip[] ships ) {
+        this.ships = ships;
+    }
 
-	public Board getBoard() {
-		return board;
-	}
+    public Board getBoard() {
+        return board;
+    }
 
-	public void setBoard(Board board) {
-		this.board = board;
-	}
+    public void setBoard( Board board ) {
+        this.board = board;
+    }
 
-	public int getDestroyedCount() {
-		return destroyedCount;
-	}
+    public int getDestroyedCount() {
+        return destroyedCount;
+    }
 
-	public void setDestroyedCount(int destroyedCount) {
-		this.destroyedCount = destroyedCount;
-	}
+    public void setDestroyedCount( int destroyedCount ) {
+        this.destroyedCount = destroyedCount;
+    }
 
-	public boolean isLose() {
-		return lose;
-	}
+    public boolean isLose() {
+        return lose;
+    }
 
-	public void setLose(boolean lose) {
-		this.lose = lose;
-	}
+    public void setLose( boolean lose ) {
+        this.lose = lose;
+    }
 }
--- src/main/java/ensta/util/Orientation.java
+++ src/main/java/ensta/util/Orientation.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Random;
 
 public enum Orientation {
-	SOUTH(1), NORTH(-1), WEST(-1), EAST(1);
+	SOUTH(1), NORTH(-1), WEST(-1), EAST(1), NULL(0);
 
 	private static final List<Orientation> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
 	private static final int SIZE = VALUES.size();
@@ -23,5 +23,19 @@ public enum Orientation {
 
 	public static Orientation randomOrientation() {
 		return VALUES.get(RANDOM.nextInt(SIZE));
+	}
+
+	public static Orientation StringToOrientation( String orientationName ) {
+		if ( orientationName == "south" ) {
+			return Orientation.SOUTH;
+		} else if ( orientationName == "north" ) {
+			return Orientation.NORTH;
+		} else if ( orientationName == "west" ) {
+			return Orientation.WEST;
+		} else if ( orientationName == "east" ) {
+			return Orientation.EAST;
+		} else {
+			return Orientation.NULL;
+		}
 	}
 }
